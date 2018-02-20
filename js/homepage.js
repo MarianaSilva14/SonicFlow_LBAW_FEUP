8b59c8a460e07c4ee89ec2397f48c56369b40b16
edramos-97 <up201505779@fe.up.pt>
2018-02-20 18:39:20 +0000

    contacts page with footer and header

--- js/homepage.js
+++ js/homepage.js
@@ -4,3 +4,21 @@ function menuAction(e){
 }
 
 document.querySelector("#menu-toggle").onclick = menuAction;
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  let className = "header-short";
+  if (document.body.scrollTop >= 60 || document.documentElement.scrollTop >= 60){
+    document.querySelector("header").classList.add(className);
+    document.querySelector("body").classList.add(className);
+    document.querySelector("#logo-short").classList.add(className);
+    document.querySelector("#logo").classList.add(className);
+  }
+  else if(document.body.scrollTop < 60 || document.documentElement.scrollTop < 60){
+    document.querySelector("header").classList.remove(className);
+    document.querySelector("body").classList.remove(className);
+    document.querySelector("#logo-short").classList.remove(className);
+    document.querySelector("#logo").classList.remove(className);
+  }
+}

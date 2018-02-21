2789ff577b19acb5d37da4b642bba8983b3dec42
up201506197 <up201506197@fe.up.pt>
2018-02-21 17:37:06 +0000

    FAQ page with header and footer

--- js/faqpage.js
+++ js/faqpage.js
@@ -1,8 +1,23 @@
-$(function() {
-    // Since there's no list-group/tab integration in Bootstrap
-    $('.list-group-item').on('click',function(e){
-     	  var previous = $(this).closest(".list-group").children(".active");
-     	  previous.removeClass('active'); // previous list-item
-     	  $(e.target).addClass('active'); // activated list-item
-   	});
-});
+// $(function() {
+//     // Since there's no list-group/tab integration in Bootstrap
+//     $('.list-group-item').on('click',function(e){
+//      	  var previous = $(this).closest(".list-group").children(".active");
+//      	  previous.removeClass('active'); // previous list-item
+//      	  $(e.target).addClass('active'); // activated list-item
+//    	});
+// });
+
+// function begin(){
+//   let listGroupItem = document.querySelectorAll(".list-group-item");
+//
+//   for(let i =0; i < listGroupItem.length; i++){
+//     listGroupItem[i].addEventListener('click', function(evt){
+//       var previous = document.querySelector(".list-group .active");
+//       previous.classList.remove("active");
+//       evt.target.classList.add("active");
+//     });
+//   }
+// }
+//
+//
+// begin();

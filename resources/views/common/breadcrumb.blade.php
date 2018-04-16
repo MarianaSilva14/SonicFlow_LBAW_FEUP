5b8f0e5e81046adf82aed2fa72cb4a4e5e328ccd
edramos-97 <up201505779@fe.up.pt>
2018-04-16 13:29:00 +0100

    ja arranjei mariana!!

--- resources/views/common/breadcrumb.blade.php
+++ resources/views/common/breadcrumb.blade.php
@@ -1,8 +1,6 @@
 <nav class="breadcrumb">
   @if ( $currPage != 'Homepage')
-  <a class="breadcrumb-item" href="{{url('homepage')}}">Homepage</a>
+    <a class="breadcrumb-item" href="{{url('homepage')}}">Homepage</a>
+    <span class="breadcrumb-item active">{{$currPage}}</span>
   @endif
-
-  <span class="breadcrumb-item active">{{$currPage}}</span>
-
 </nav>

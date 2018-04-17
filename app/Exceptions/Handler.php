915bd38e1dc76810c6f7415b63d7641b8a534af5
edramos-97 <up201505779@fe.up.pt>
2018-04-17 15:05:50 +0100

    simple Exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\Access\AuthorizationException as AuthorizationException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,6 +49,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof NotFoundHttpException) {
+          return redirect()->route('homepage');
+        }
+
+        if($exception instanceof AuthorizationException){
+          return redirect()->route('login');
+        }
+
         return parent::render($request, $exception);
     }
 }

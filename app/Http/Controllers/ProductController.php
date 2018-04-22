5ffb03d9eb3e71ce31ce50449b0084fe7020199f
edramos-97 <up201505779@fe.up.pt>
2018-04-22 16:52:33 +0100

    fixed merge

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,7 +19,6 @@ use phpDocumentor\Reflection\Types\Integer;
 class ProductController extends Controller
 {
 
-<<<<<<< HEAD
   /**
    * Gets products based on filters.
    *
@@ -117,139 +116,9 @@ class ProductController extends Controller
     $product->price = $request->input('price');
     $product->discountprice = $request->input('discountPrice');
     $product->stock = $request->input('stock');
-=======
-    /**
-     * Gets products based on filters.
-     *
-     * @param Request $request
-     * @return JSON The products.
-     */
-    public function getProducts(Request $request)
-    {
-        $query = DB::table('product');
-
-        $catgoryID = intval($request->input('categoryID'));
-        if ($catgoryID != null) {
-            $query = $query->where('category_idCat', $catgoryID);
-        }
-
-        $minPrice = floatval($request->input('minPrice'));
-        $maxPrice = floatval($request->input('maxPrice'));
-        if ($minPrice != null && $maxPrice != null && ($minPrice < $maxPrice)) {
-            $query = $query->whereBetween('price', [$minPrice, $maxPrice]);
-        }
-
-        $available = filter_var($request->input('productAvailability'), FILTER_VALIDATE_BOOLEAN);
-        if ($available != null) {
-            if ($available) {
-                $query = $query->where('stock', '>', 0);
-            } else {
-                $query = $query->where('stock', '=', 0);
-            }
-        }
-
-        $title = $request->input('title');
-        if ($title != null) {
-            $query = $query
-                ->whereRaw('search @@ plainto_tsquery(\'english\',?)', [$title])
-                ->orderByRaw('ts_rank(search,  plainto_tsquery(\'english\',?) DESC', [$title]);
-        }
-
-        $products = $query->get();
-
-        //+title:String Category
-        //+categoryID:Integer 	Category
-        //+productBrand:String 	Product Brand
-        //+minPrice:Integer 	Price Lower Bound
-        //+maxPrice:String 	Price Higher Bound
-        //+productAvailability:boolean 	Product Availability
-
-
-        return json_encode($products);
-    }
-
-    public function getProductBySku(Integer $sku)
-    {
-        $product = DB::table('product')->where('sku', $sku)->first();
-        return json_encode($product);
-    }
-
-    public function getProductsByName(String $title)
-    {
-        $products = DB::table('product')
-            ->whereRaw('search @@ plainto_tsquery(\'english\',?)', [$title])->get();
-        return json_encode($products);
-    }
-
-    public function getDiscounted()
-    {
-        $discounted_products = DB::table('product')->whereNotNull('discountprice')->get();
-        return json_encode($discounted_products);
-    }
-
-    public function editForm($sku)
-    {
-        try {
-            $this->authorize('edit', Product::class);
-        } catch (Exception $e) {
-            return redirect('homepage');
-        }
-        $product = Product::find($sku);
-        return view('pages.editProduct', ['product' => $product,
-            'attributes' => $product->attributes(),
-            'categories' => DB::table('category')->get()
-        ]);
-    }
-
-    public function update(Request $request, $sku)
-    {
-        try {
-            $this->authorize('edit', Product::class);
-        } catch (Exception $e) {
-            return redirect('homepage');
-        }
-
-        $product = Product::find($sku);
-
-        if ($request->hasFile('pictures')) {
-            $files = $request->file('pictures');
-            foreach ($files as $key => $file) {
-                if ($key == 1) {
-                    $picPath .= $request->file('picture')->store('public/' . $sku, ['public']);
-                } else {
-                    $picPath .= ';' . $request->file('picture')->store('public/' . $sku, ['public']);
-                }
-            }
-            $product->picture = $picPath;
-        }
-
-        $product->title = $request->input('title');
-        $product->description = $request->input('description');
-        $product->price = $request->input('price');
-        $product->discountprice = $request->input('discountPrice');
-        $product->stock = $request->input('stock');
-
-        $product->save();
-
-
-        $attributes_to_update = $request->input('attributes');
-        foreach ($attributes_to_update as $attribute_id => $attribute_value) {
-            if ($attribute_value == '')
-                $attribute_value = 'NA';
-
-            DB::table('attribute_product')
-                ->where('product_idproduct', $sku)
-                ->where('attribute_idattribute', $attribute_id)
-                ->update(['value' => $attribute_value]);
-        }
->>>>>>> d896db8663157ddb4cd4729ef4e9d5148590d502
 
     $product->save();
 
-<<<<<<< HEAD
-=======
-        return view('pages.product', ['product' => $product, 'attributes' => $product->attributes()]);
->>>>>>> d896db8663157ddb4cd4729ef4e9d5148590d502
 
     $attributes_to_update = $request->input('attributes');
     foreach ($attributes_to_update as $attribute_id => $attribute_value){
@@ -257,7 +126,6 @@ class ProductController extends Controller
         $attribute_value = 'N/A';
       $product->setAttribute($attribute_id,$attribute_value);
     }
-<<<<<<< HEAD
       return view('pages.product', ['product' => $product, 'attributes' => $product->attributes() ]);
   }
 
@@ -298,47 +166,4 @@ class ProductController extends Controller
 
       return url('product', ['id' => $sku ]);
   }
-=======
-
-    public function show($sku)
-    {
-        $product = Product::find($sku);
-        return view('pages.product', ['product' => $product, 'attributes' => $product->attributes()]);
-    }
-
-    public function create()
-    {
-        try {
-            $this->authorize('createNewProduct', Product::class);
-        } catch (Exception $e) {
-            return redirect('homepage');
-        }
-        return view('pages.addProduct', ['categories' => DB::table('category')->get()]);
-    }
-
-    public function addComment($sku, Request $request)
-    {
-        $user = Auth::user();
-        $comment_text = $request->input('commentary');
-
-        $comment = Comment::create([
-            'user_username' => $user->username,
-            'commentary' => $comment_text,
-            'flagsno' => 0,
-            'product_idproduct' => $sku
-        ]);
-
-        $parent_id = intval($request->input('parent_id'));
-        if ($parent_id != null) {
-            $child_id = $comment->id;
-
-            Answer::create([
-                'comment_idparent' => $parent_id,
-                'comment_idchild' => $child_id
-            ]);
-        }
-
-        return url('product', ['id' => $sku]);
-    }
->>>>>>> d896db8663157ddb4cd4729ef4e9d5148590d502
 }
--- app/Product.php
+++ app/Product.php
@@ -23,7 +23,6 @@ class Product extends Model
    */
   protected $table = 'product';
 
-<<<<<<< HEAD
   public function favoritesList(){
     return $this->belongsToMany('App\Customer','favorite','user_username', 'product_idproduct');
   }
@@ -69,50 +68,6 @@ class Product extends Model
             ->get();
     return $comments;
   }
-=======
-    public function favoritesList(){
-        return $this->belongsToMany('App\Customer','favorite','user_username', 'product_idproduct');
-    }
-
-    public function attributes(){
-        $attributes =
-            DB::table('category')
-                ->join('category_attribute',function($join){
-                    $join->on('category.id','=','category_idcategory')
-                        ->where('category.id','=',$this->category_idcat);
-                })
-                ->join('attribute_product',function($join){
-                    $join->on('attribute_product.attribute_idattribute','=','category_attribute.attribute_idattribute')
-                        ->where('attribute_product.product_idproduct','=',$this->sku);
-                })
-                ->join('attribute','attribute.id','=','attribute_product.attribute_idattribute')
-                ->select('attribute.name','attribute_product.value','attribute.id')
-                ->get();
-        return $attributes;
-    }
-
-    public function comments(){
-        return $this->hasMany('App\Comment','product_idproduct');
-    }
-
-    public function getTopComments(){
-        // $comments =
-        // DB::table('comment')
-        //   ->where('comment.product_idproduct','=',$this->sku)
-        //   ->whereRaw('comment.id NOT IN (SELECT comment_idchild FROM answer)')
-        //   ->get();
-
-        $comments =
-            Comment::where('comment.product_idproduct','=',$this->sku)
-                ->whereRaw('comment.id NOT IN (SELECT comment_idchild FROM answer)')
-                ->get();
-        return $comments;
-    }
-
-    public function getImages(){
-        return explode(';',$this->picture);
-    }
->>>>>>> d896db8663157ddb4cd4729ef4e9d5148590d502
 
   public function getImages(){
     return explode(';',$this->picture);

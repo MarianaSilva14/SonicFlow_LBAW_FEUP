4d98c2f8ccd5d0b8544c9bd465a9048589356b1f
Xavier Fontes <xfontes42@gmail.com>
2018-04-25 18:03:46 +0100

    updated partial and other stuff

--- resources/views/partials/product_mini.blade.php
+++ resources/views/partials/product_mini.blade.php
@@ -2,30 +2,33 @@
     <div class="ibox">
         <div class="ibox-content product-box">
             <div class="product-imitation">
-                <img src="https://static.fnac-static.com/multimedia/Images/PT/NR/01/41/12/1196289/1505-1/tsp20170623090030/Apple-MacBook-Pro-15-Retina-i7-2-8GHz-16GB-1TB-Radeon-Pro-555-com-Touch-Bar-e-Touch-ID-Cinzento-Sideral.jpg" alt="194x228"class="img-fluid">
+                <img src="{{ Storage::url($product->picture) }}" alt="Image for {{ $product->title }}"class="img-fluid">
             </div>
             <div class="product-desc">
                 <div class="priceTags">
-                          <span class="bg-danger discount-price">
-                            3199,99€
+                        @if( $product->discountprice != null)
+                        <span class="bg-danger discount-price">
+                            {{ $product->discountprice }}
                           </span>
+                        @endif
+
                     <span class="product-price">
-                            3622,67€
+                            {{ $product->price }}
                           </span>
                 </div>
                 <br>
-                <small class="text-muted">Category</small>
-                <a href="{{route('product',['id' => '186596482'])}}" class="product-name"> Apple MacBook Pro 15''</a>
+                <small class="text-muted">Category {{ $product->category_idcat }}</small>
+                <a href="{{route('product',['id' => $product->sku])}}" class="product-name">{{ $product->title }}</a>
 
                 <div class="small m-t-xs">
-                    Many desktop publishing packages and web page editors now.
+                    {{ $product->description }}
                 </div>
                 <br>
                 <div class="m-t text-righ row">
                     <a href="#" class="addtoCart col-8 btn btn-xs btn-outline btn-primary">Add to cart  <i class="fas fa-cart-plus"></i></a>
                     {{--TODO: Change this to ajax call later--}}
                     <a class="addtoFavs col-3 btn btn-xs btn-outline btn-primary">
-                        <form method="post" action="{{route('toggleFavoritesList', ['sku' => '83108184'])}}">
+                        <form method="post" action="{{route('toggleFavoritesList', ['sku' => $product->sku])}}">
                             {{csrf_field()}}
                             <button style = "background-color: transparent;border-color: transparent;color: white" role="submit"><i class="far fa-heart"></i></button>
                         </form>

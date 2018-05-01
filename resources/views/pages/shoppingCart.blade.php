2e8c7b1c89f639f226fe3da9151507c7e2ecedbc
Xavier Fontes <xfontes42@gmail.com>
2018-05-01 11:55:02 +0100

    added discount price to purchase

--- resources/views/pages/shoppingCart.blade.php
+++ resources/views/pages/shoppingCart.blade.php
@@ -31,9 +31,22 @@
               <td class="delete_cart removeSingle" data-id="{{$products[$i]->sku}}"><i class="far fa-trash-alt fa-2x"></i></td>
               <td class="productImg"><img src="{{Storage::url($products[$i]->getImages()[0])}}" alt="product thumbnail" class="img-fluid"/></td>
               <td>{{$products[$i]->title}}</td>
-              <td class="unitCost">{{$products[$i]->price}}€</td>
+              <td class="unitCost">
+                  @if($products[$i]->discountprice != "")
+                      {{$products[$i]->discountprice}}
+                  @else
+                      {{$products[$i]->price}}
+                  @endif
+                  €</td>
               <td class="amount"><input type="number" class="form-control text-center" min=1 max={{$products[$i]->stock}} value="{{$values[$i]}}"></td>
-              <td class="totalCost">{{$values[$i]*$products[$i]->price}}€</td>
+              <td class="totalCost">
+                  @if($products[$i]->discountprice != "")
+                      {{$values[$i]*$products[$i]->discountprice}}
+                  @else
+                      {{$values[$i]*$products[$i]->price}}
+                  @endif
+                  €
+              </td>
             </tr>
           @endfor
           <tfoot>
@@ -47,8 +60,11 @@
 							<td class="hidden-xs text-center">
                 <strong class="checkoutCost">
                   <?php $total=0; for($i=0; $i < count($products); $i++) {
-                    $total += $products[$i]->price*$values[$i];
-                  } echo $total?>€
+                      if($products[$i]->discountprice != "")
+                          $total += $products[$i]->discountprice*$values[$i];
+                      else
+                          $total += $products[$i]->price*$values[$i];
+                    } echo $total?>€
                 </strong>
                 <a href="" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a>
               </td>

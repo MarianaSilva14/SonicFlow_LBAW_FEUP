271ac868c3d2c777867ad59e43a5b1ae5c97951c
edramos-97 <up201505779@fe.up.pt>
2018-05-17 09:15:47 +0100

    comment using deleted account

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -1,14 +1,21 @@
 <div class="@if($reply) comment-reply @else comment @endif col-md-11 offset-md-1 col-sm-10 offset-sm-2">
   <div class="row">
     <div class="comment-avatar col-md-1 col-sm-2 text-center pr-1">
-      @if($comment->user->getPicture()=="")
+      @if($comment->user == null || $comment->user->getPicture()=="")
         <img class="mx-auto rounded-circle img-fluid" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" alt="avatar">
       @else
         <img class="mx-auto rounded-circle img-fluid" src="{{\Illuminate\Support\Facades\Storage::url($comment->user->getPicture())}}" alt="avatar">
       @endif
     </div>
     <div class="comment-content col-md-11 col-sm-10 col-12">
-      <h6 class="small comment-meta"><span style="color:#007bff" class="disabled">{{$comment->user->username}}</span> {{Carbon\Carbon::parse($comment->date)->diffForHumans()}}</h6>
+      <h6 class="small comment-meta">
+        <span style="color:#007bff" class="disabled">
+          @if($comment->user == null)
+            Deleted Account
+          @else
+            {{$comment->user->username}}
+          @endif
+        </span> {{Carbon\Carbon::parse($comment->date)->diffForHumans()}}</h6>
       <div class="comment-body">
         <p>
           @if($comment->deleted)

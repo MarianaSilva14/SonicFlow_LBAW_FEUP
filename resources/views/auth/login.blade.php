00014c828d4f84d93439ef1797a5195e098c8275
MarianaSilva14 <mariana16095@gmail.com>
2018-05-29 17:47:07 +0100

    update layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,13 +31,13 @@
                 <input type="checkbox" value="remember-me"> Remember me
             </label>
         </div>
-            <div class="g-signin2" data-onsuccess="onSignIn"></div>
         <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
         <a href="{{ route('register') }}" class="btn btn-lg btn-primary btn-block btn-register" >Register</a>
       </form>
       <a href="{{ route('recoverPasswordConfirmation') }}" class="forgot-password">
           Forgot the password?
       </a>
+      <div class="g-signin2" data-onsuccess="onSignIn"></div>
   </div>
 </div>
 @endsection

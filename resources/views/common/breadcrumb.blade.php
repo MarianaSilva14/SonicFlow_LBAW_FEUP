486ab0f371d5f04a1030f3f35f6d85bb1584b827
edramos-97 <up201505779@fe.up.pt>
2018-05-29 12:17:30 +0100

    improved help button position

--- resources/views/common/breadcrumb.blade.php
+++ resources/views/common/breadcrumb.blade.php
@@ -5,7 +5,7 @@
  @endif
 
  @if ( $currPage == 'My Profile')
- <i class="fas fa-question-circle" style="text-align:right;"  data-target="#exampleModalCenter" data-toggle="modal"></i>
+ <i id="help" class="fas fa-question-circle breadcrumb-item active" data-target="#exampleModalCenter" data-toggle="modal"></i>
  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
@@ -15,7 +15,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body"> 
+      <div class="modal-body">
         <p>My Profile</p>
 
         <p>Page to consult your customer profile of the online store "Sonic Flow". Here you can view and edit your profile, as well as check your shopping history and articles placed in favorites</p>
@@ -31,8 +31,7 @@
   </div>
 </div>
 @elseif( $currPage == 'Homepage')
-
-<i class="fas fa-question-circle" style="float:right;" data-target="#exampleModalCenter" data-toggle="modal"></i>
+<i id="help" class="fas fa-question-circle breadcrumb-item active" data-target="#exampleModalCenter" data-toggle="modal"></i>
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
@@ -42,13 +41,13 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body"> 
+      <div class="modal-body">
         <p>Home</p>
         <p>Homepage for the "Sonic Flow" online store. Here you can buy various technological products, compare products and even build your own pc suited to your needs. </p>
       At the beginning we highlight the products on sale. We have other options available in the left side menu: </p>
       <p>a) In the configurator customize your own pc.</p>
       <p>b) See the products of each category.</p>
-      <p>For more information or if you notice any irregularities please  <a href="{{route('contact')}}">Contact us</a>. </p> 
+      <p>For more information or if you notice any irregularities please  <a href="{{route('contact')}}">Contact us</a>. </p>
     </div>
     <div class="modal-footer">
     </div>
@@ -57,7 +56,7 @@
 </div>
 @elseif( $currPage == 'List Products')
 
-<i class="fas fa-question-circle" style="float:right;" data-target="#exampleModalCenter" data-toggle="modal"></i>
+<i id="help" class="fas fa-question-circle breadcrumb-item active" data-target="#exampleModalCenter" data-toggle="modal"></i>
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
@@ -67,7 +66,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body"> 
+      <div class="modal-body">
         <p>List Products</p>
         <p>Page to view products of the store by category.</p>
         <p>We have other options available in the left side menu:</p>
